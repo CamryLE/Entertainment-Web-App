592e371ba5a7c37c2b7d748e8ed91a9319bb633d
Camry <106214553+CamryLE@users.noreply.github.com>
2023-04-24 13:03:33 -0400

    Add styled navbar icons

--- movie-app/src/components/Navbar.js
+++ movie-app/src/components/Navbar.js
@@ -1,19 +1,46 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Profile from '../assets/image-avatar.png'
-
+import MovieIcon from '../assets/icon-nav-movies.svg'
+import TVIcon from '../assets/icon-nav-tv-series.svg'
+import Gallery from '../assets/icon-nav-home.svg'
+import BookMark from '../assets/icon-nav-bookmark.svg'
+import Logo from '../assets/logo.svg'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <div className='refresh'> </div>
+      <div className='refresh'>
+        <Link to={`/`}>
+        <img src={Logo} className='logo'>
+              </img>
+        </Link>
+      </div>
       <div className='categories'>
-        <ul className='category-list'>
+        <ul style={{ listStyleType: "none" }} className='category-list'>
           <li className='category-icon'>
-          <Link to={`movies`}>
-            <img src='icons/' className='icon'>
-            </img>
-          </Link>
+            <Link to={`/`}>
+              <img src={Gallery} className='icon'>
+              </img>
+            </Link>
+          </li>
+          <li className='category-icon'>
+            <Link to={`TV`}>
+              <img src={MovieIcon} className='icon'>
+              </img>
+            </Link>
+          </li>
+          <li className='category-icon'>
+            <Link to={`movies`}>
+              <img src={TVIcon} className='icon'>
+              </img>
+            </Link>
+          </li>
+          <li className='category-icon'>
+            <Link to={`bookmark`}>
+              <img src={BookMark} className='icon'>
+              </img>
+            </Link>
           </li>
         
         </ul>
@@ -21,7 +48,7 @@ const Navbar = () => {
       
       <div className='profile'>
         <Link to={`profile`}>
-          <img src={Profile}/>
+          <img id='pfp' src={Profile}/>
         </Link>
       </div>
       
